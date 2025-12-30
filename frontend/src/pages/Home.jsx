ebf53aa92bcf9b96284ae47cb452c8f83a029d65
bhanu-3333 <bhanuu339@gmail.com>
2025-12-30 22:58:49 +0530

    frontend UI for disease prediction and AI doctor pending

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,7 +3,6 @@ export default function Home() {
     <div className="card">
       <h1>AI Health Assistant</h1>
       <p>
-        
         MediScan AI uses Machine Learning and Artificial Intelligence to predict
         diseases and provide instant medical guidance.
       </p>
