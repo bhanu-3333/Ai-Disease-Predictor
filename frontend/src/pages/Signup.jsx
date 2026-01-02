5a8901be75cef6df2d613cd4195db2ac4e6c68eb
bhanu-3333 <bhanuu339@gmail.com>
2026-01-02 21:48:36 +0530

    maked responsive

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -11,7 +11,7 @@ export default function Signup() {
   const [password, setPassword] = useState("");
 
   const signup = async () => {
-    const res = await fetch("http://127.0.0.1:5000/signup", {
+    const res = await fetch("https://ai-disease-predictor-4.onrender.com/signup", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ name, age, gender, email, password })
